240fa417752ef6d589df8b49f917b68586b7e936
Ryan Yang <javathon@gmail.com>
2018-05-25 11:45:58 -0700

    autocomplete search bar implements 2 treemaps to link stock information together and to make suggestions faster

--- AutocompleteSearch.java
+++ AutocompleteSearch.java
@@ -1,13 +1,22 @@
 package tech.ryanqyang;
 
+import javax.swing.*;
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.TreeMap;
 
-public class AutocompleteSearch{
-    ArrayList
+public class AutocompleteSearch extends JComboBox<String>{
+    private TreeMap<String, String> companyToSymbol;
+    private TreeMap<String, String> symbolToCompany;
+
     public AutocompleteSearch(){
+        super(new String[]{});
+        companyToSymbol = new TreeMap<>();
+        symbolToCompany = new TreeMap<>();
+        retrieveList();
     }
 
     /**
@@ -27,7 +36,7 @@ public class AutocompleteSearch{
                     for(int i = 2; i < line.length(); i++){
                         if(line.charAt(i) == '"'){
                             if(counter == 0){
-                                p.print(line.substring(1, i) + " - ");
+                                p.print(line.substring(1, i) + ":");
                                 counter = 1;
                             }else if(counter == 1){
                                 secondIndex = i + 1;
@@ -43,6 +52,36 @@ public class AutocompleteSearch{
                 e.printStackTrace();
             }
         }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Takes formatted stocks and symbols from generated file from parse() and puts them inside 2 tree maps
+     * they map the stocks together in sorted order so autocomplete can work best
+     *
+     */
+    public void retrieveList(){
+        File listFile = new File("/Users/ryanyang/Desktop/Workspace/CS003B/StockVisualizer/src/tech/ryanqyang/stocksList.txt");
+        try(Scanner sc = new Scanner(listFile)){
+            while(sc.hasNext()) {
+                String stockMeta = sc.nextLine();
+                findColon:
+                for (int i = 0; i < stockMeta.length(); i++) {
+                    if (stockMeta.charAt(i) == ':') {
+                        System.out.println(stockMeta.substring(i + 1));
+
+                        //reassigning string creates a new reference but old reference is kept because of the maps
+                        String symb = stockMeta.substring(0, i).trim();
+                        String company = stockMeta.substring(i + 1);
+                        symbolToCompany.put(symb, company);
+                        companyToSymbol.put(company, symb);
+                        break findColon;
+                    }
+                }
+            }
+        }catch(IOException e){
+            //shouldn't be triggered unless user removed program generated list from parse
             e.printStackTrace();
         }
     }
